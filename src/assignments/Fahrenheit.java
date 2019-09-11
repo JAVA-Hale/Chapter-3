956dfe2ae50f5e9c38609d0abef980a45e98221c
Thomas hale <TH146688@davenportschools.org>
2019-09-11 09:49:53 -0500

    update change

---
+++ src/assignments/Fahrenheit.java
@@ -0,0 +1,19 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class Fahrenheit {
+
+    public static void main(String[] args){
+        double temp;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your temp at 8:00 am >>> ");
+        temp = (int) input.nextDouble();
+        caculateToCelsius();
+    }
+
+    public static void caculateToCelsius(int temp) {
+        double Celsius;
+        Celsius = (temp - 32)* 5/9;
+    }
+}
--- src/assignments/MetricConversion.java
+++ src/assignments/MetricConversion.java
@@ -1,9 +1,35 @@
 package assignments;
 
+import java.util.Scanner;
+
 public class MetricConversion {
 
     public static void main(String[] args){
-         double centimeters = 2.54;
-         double liters = 3.7874;
+        int numInches;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number of inches >>> ");
+        numInches = keyboard.nextInt();
+        caculateToCentimeters(numInches);
+
+        int numLiters;
+
+        System.out.println("Enter a number of gallons >>> ");
+        numLiters = keyboard.nextInt();
+        caculateToLiters(numLiters);
+
+    }
+    public static void caculateToCentimeters(int numinches){
+        double numCentimeters;
+        final double NUM_CENTIMETERS_IN_INCHES = 2.54;
+        numCentimeters = (numinches / NUM_CENTIMETERS_IN_INCHES);
+
+        System.out.println("The number of centimeters is " + numCentimeters + "");
+    }
+    public static void caculateToLiters(double numLiters){
+        final double NUM_LITERS_IN_GALLONS = 3.7854;
+        numLiters = (numLiters / NUM_LITERS_IN_GALLONS);
+
+        System.out.println("The number of centimeters is " + numLiters + "");
     }
 }
