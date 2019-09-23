3d17f037299f487d92fb8fec2e44584ae6abaa58
Thomas hale <TH146688@davenportschools.org>
2019-09-23 08:32:12 -0500

    update change

--- src/assignments/Fahrenheit.java
+++ src/assignments/Fahrenheit.java
@@ -6,14 +6,19 @@ public class Fahrenheit {
 
     public static void main(String[] args){
         double temp;
+        double Celsius = 32;
         Scanner input = new Scanner(System.in);
         System.out.println("What is your temp at 8:00 am >>> ");
         temp = (int) input.nextDouble();
+        caculateToCelsiusint (temp, Celsius);
+    }
+
+    public static void caculateToCelsiusint(double temp, double Celsius) {
+        temp = (temp - 32);
+
+        System.out.println("It is " + temp + " celsius");
+    }
+
 
     }
 
-    public static void caculateToCelsius(int temp) {
-        double Celsius;
-        Celsius = (temp - 32)* 5/9;
-    }
-}
